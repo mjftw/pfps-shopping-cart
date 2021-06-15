2a40dbf2443cb979d892469169bc79cf9ddb0ce3
Merlin Webster <merlin.webster@kaluza.com>
2021-06-15 07:41:14 +0100

    Removed all scala sources, leaving only filenames

--- modules/core/src/main/scala/retry/PolicyDecision.scala
+++ modules/core/src/main/scala/retry/PolicyDecision.scala
@@ -1,13 +1,1 @@
-package retry
 
-import scala.concurrent.duration.FiniteDuration
-
-sealed trait PolicyDecision
-
-object PolicyDecision {
-  case object GiveUp extends PolicyDecision
-
-  final case class DelayAndRetry(
-      delay: FiniteDuration
-  ) extends PolicyDecision
-}
--- modules/core/src/main/scala/shop/http/routes/version.scala
+++ modules/core/src/main/scala/shop/http/routes/version.scala
@@ -1,5 +1,1 @@
-package shop.http.routes
 
-object version {
-  val v1 = "/v1"
-}
